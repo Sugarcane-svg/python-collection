7fc655d9211c016fc5153dc15c11687b110ff1da
phoebe <jzou89260@gmail.com>
2022-01-18 21:02:10 -0500

    practice

---
+++ MaxOfThree.py
@@ -0,0 +1,13 @@
+# implement a function that takes as input three variables, and returns the largest of the three. Do this without using the Python max() function
+
+def get_max(a,b,c):
+    if a > b and a > c:
+        return a
+    elif b > a and b > c:
+        return b
+    elif c > a and c > b:
+        return c
+
+if __name__ == '__main__':
+    a,b,c = 1,2,3
+    print(get_max(a,b,c))
