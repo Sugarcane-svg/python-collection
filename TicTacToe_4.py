0f0a15b563f335fd5a302251cc1c5989d51d0542
phoebe <jzou89260@gmail.com>
2022-01-26 16:34:47 -0500

    make the game working

--- TicTacToe_4.py
+++ TicTacToe_4.py
@@ -1,4 +1,4 @@
-# make the whole game. Reuse the code from practicepython.org
+# make the whole game. modify and reuse the code from practicepython.org
 
 # draw board
 def drawboard(board):
@@ -12,31 +12,32 @@ def drawboard(board):
         print(board)
 
 # check winner
+import numpy as np
 def checkGrid(grid):
 	# rows
-	for x in range(0, 3):
+	for x in range(0,3):
 		row = set([grid[x][0], grid[x][1], grid[x][2]])
-		if len(row) == 1 and grid[x][0] != 0:
-			return grid[x][0]
+		if len(row) == 1 and grid[x][1] != " ":
+			return grid[x][1]
 
 	# columns
 	for x in range(0, 3):
 		column = set([grid[0][x], grid[1][x], grid[2][x]])
-		if len(column) == 1 and grid[0][x] != 0:
+		if len(column) == 1 and grid[0][x] != " ":
 			return grid[0][x]
 
 	# diagonals
 	diag1 = set([grid[0][0], grid[1][1], grid[2][2]])
 	diag2 = set([grid[0][2], grid[1][1], grid[2][0]])
-	if len(diag1) == 1 or len(diag2) == 1 and grid[1][1] != 0:
+	if len(diag1) == 1 or len(diag2) == 1 and grid[1][1] != " ":
 		return grid[1][1]
 
 	return 0
 
 def init_game():
-    return [['0', '0', '0'],
-            ['0', '0', '0'],
-            ['0', '0', '0']]
+    return [[' ', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' ']]
 
 
 def get_steps(s):
@@ -44,39 +45,57 @@ def get_steps(s):
 
 
 def move(l, steps, player):
-    if l[int(steps[0])-1][int(steps[1])-1] == 0 and player == 1:
-        l[int(steps[0])-1][int(steps[1])-1] = 'X'
-    elif l[int(steps[0])-1][int(steps[1])-1] == 0 and player == 2:
-        l[int(steps[0])-1][int(steps[1])-1] = 'O'
+    a = int(steps[0])
+    b = int(steps[1])
+
+    if l[a-1][b-1] == ' ' and player == 1:
+        l[a-1][b-1] = 'X'
+    elif l[a-1][b-1] == ' ' and player == 2:
+        l[a-1][b-1] = 'O'
     else:
         return -1
 
     return l
 
-# def player(l, player):
-#     play = ''
-#     if player == 1:
-#         play = input('enter a coordinate separate by "," for player1: ')
-#     else:
-#         play = input('enter a coordinate separate by "," for player2: ')
+def player(l, player):
+    play = ''
+    if player == 1:
+        play = input('enter a coordinate separate by "," for player1: ')
+    else:
+        play = input('enter a coordinate separate by "," for player2: ')
 
-#     steps = get_steps(play)
-#     moves = move(l, steps, player)
-#     drawboard(moves)
+    steps = get_steps(play)
+    return move(l, steps, player)
 
-import TicTacToe_3 as p
+def display_winner(game):
+    if checkGrid(game) == 'X':
+            return "player1 win"
+    elif checkGrid(game) == 'O':
+            return 'player2 win'
+    else:
+        return 0
+
 if __name__ == "__main__":
-    total_moves = 9
+    total_step = 9
+
     game = init_game()
-    drawboard(game)
+    while total_step > 0:
+        # player1 = input('enter a coordinate separated by "," for player 1: ')
+        # steps = get_steps(player1)
+        # print('step: ' + str(steps))
+        # game = move(game, steps, 1)
+        game = player(game, 1)
+        total_step -=1
+        drawboard(game)
+        if display_winner(game) != 0:
+            break  
+        game = player(game, 2)
+        total_step -= 1
+        drawboard(game)
+        if display_winner(game) != 0:
+            break
     
-    # moves = move(game, get_steps(player1), 1)
-    # print(moves)
-    # while total_moves > 0:
-    #      player(game, 1)
-    #      total_moves -= 1
-    #      if total_moves == 0:
-    #         break
-    #      player(game, 2)
-    #      total_moves -= 1
-    # print('game board is full, the game is finished')
+    print(display_winner(game))
+
+
+        
