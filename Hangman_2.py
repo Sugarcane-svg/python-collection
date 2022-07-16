f527e430741f237c264b86865a528bb33f6fc24d
phoebe <jzou89260@gmail.com>
2022-07-16 00:15:54 -0400

    change s to list for later change the underscore into letter, string is immutable

--- Hangman_2.py
+++ Hangman_2.py
@@ -5,31 +5,33 @@
 # write the logic that asks a player to guess a letter and displays letters in the clue word that were guessed correctly.bonus: keep track of the letters the player guessed and display a different massage of the player tries to guess the same letter again.
 
 
+from re import A
 import Hangman_1 as h1
 
 if __name__ == '__main__':
 
     print('Welcome to Hangman!')
     word = h1.pickword()
-    s = '_ '* len(word)
-    print(s)
+    s = ('_ '* len(word)).split()
+    print(" ".join(s))
 
     b = len(word)
     # 
     while b > 0:
-        new_guess = input('Guess your letter: ').upper
-        if new_guess not in word.upper:
+        new_guess = (input('Guess your letter(upper case only): '))
+        if word.find(new_guess) == -1:
             print("Incorrect!")
-            new_guess = input("Guess your letter: ").upper
         else:
             indexes = []
             for i in range(len(word)):
                 if word[i] == new_guess:
                     indexes.append(i)
-            
+
             for j in indexes:
-                s[2*j-1] = new_guess
-            
+                s[j] = new_guess
+
             b = b-len(indexes)
+        print(" ".join(s))
+    print("Well Done!")
             
 
